95fb16b83e3c6d12e1deacd0ae9459fb3df9d9e9
MJoy99 <trail_dog_mo@yahoo.com>
2022-04-11 01:58:12 -0400

    Updated UserThread for complete functionality.

--- src/main/java/org/ece542/Server/UserThread.java
+++ src/main/java/org/ece542/Server/UserThread.java
@@ -1,26 +1,75 @@
 package org.ece542.Server;
 
+
+import java.io.*;
 import java.net.Socket;
 
 public class UserThread extends Thread{
 
     private Socket clientSocket;
     private ServerMain serverMain;
-    private String userName;
+    private PrintWriter writer;
 
     /**
      * Constructor
-     * @param socket
-     * @param serverMain
+     * @param socket  client socket
+     * @param serverMain ServerMain used to generate the UserThread.
      */
     public UserThread(Socket socket, ServerMain serverMain) {
+        clientSocket=socket;
+        this.serverMain=serverMain;
+
     }
 
+    /**
+     * Effects: reads lines from client socket.  First line is user name. Send broadcast to <bold>ServerMain</bold> new user
+     * has connected.  Sends broadcast to <bold>ServerMain</bold> for each new line received from client socket
+     * until "!exit" is received.  sends exit notification, closes client socket and removes user from userThread
+     * Set.
+     */
     @Override
     public void run() {
-        super.run();
+
+        try {
+            InputStream inputStream = clientSocket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            OutputStream outputStream = clientSocket.getOutputStream();
+            writer = new PrintWriter(outputStream,true);
+
+            String userName = reader.readLine();
+            serverMain.addUserName(userName);
+
+            String serverMessage = userName + " has joined the chat.";
+            serverMain.sendBroadcast(serverMessage, this);
+
+            String clientMessage;
+
+            while(true){
+                clientMessage = reader.readLine();
+                if (clientMessage.equalsIgnoreCase("!exit"))
+                    break;
+                serverMessage = userName + ": " + clientMessage;
+                serverMain.sendBroadcast(serverMessage, this);
+            }
+
+            serverMain.removeUser(userName, this);
+            clientSocket.close();
+
+            serverMessage = userName + " has left the group chat";
+            serverMain.sendBroadcast(serverMessage, null);
+
+        } catch (IOException e) {
+            System.out.println("Class: UserThread. Method: run. IOExcpetion on clientSocket.getInputStream.");
+            e.printStackTrace();
+        }
     }
 
+    /**
+     * Effects: sends the message out to the client Socket.
+     * @param message String containing text of message.
+     */
     public void sendMessage(String message) {
+        writer.println(message);
     }
 }
